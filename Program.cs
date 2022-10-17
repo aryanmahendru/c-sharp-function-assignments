065603111ebe2f7bcbaa64e15bb426d5f6c984a3
aryanmahendru <114501779+aryanmahendru@users.noreply.github.com>
2022-10-17 09:45:05 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿//Functions assignment 
+
+List<int> list = new List<int>{72,3,2,1};
+//contains
+bool contains(List<int> aList ,int numfind) {
+   foreach (int b in aList) {
+      if (b == numfind) {
+         return true;
+      }
+   }
+   return false;
+}
+
+if (contains(list , 2)){
+    Console.WriteLine("2 is IN the List");  
+}
+else{
+   Console.WriteLine("2 is not in the list");
+}
+ 
+
+
+//indexof
+int indexof(List<int> cList, int nfind) {
+   int indexofnum=-1; 
+   for(int c=0;c<cList.Count;c++){
+      if (cList[c] == nfind){
+         indexofnum = c;
+         
+      }
+   }
+return indexofnum;
+}
+
+int index = indexof(list,72);
+Console.WriteLine("index of number is "+index);
+
+
+
+//indexofmin
+int indexofmin(List<int> bList){
+   int indexxx=-1;
+   int max=0;
+   for (int a=0;a<bList.Count;a++){
+       if (max<bList[a]){
+         max=bList[a];
+       }
+   }
+
+   for(int h=0;h<bList.Count;h++){
+      if(bList[h]<max){
+         indexxx=h;
+    
+      }
+   }
+   return indexxx;
+}
+Console.WriteLine("index of minimum number is "+indexofmin(list));
+
+
+//swap
+void Swap(List<int> dList,int index77,int index88){
+   int number1=dList[index77];
+   dList[index77]=dList[index88];
+   dList[index88]=number1;
+   }
+Swap(list,0,1);
+
+for(int h=0;h<list.Count;h++)
+Console.WriteLine(list[h]);
+
+
+
+//reverse
+List<int> reverse(List<int> eList){
+   List<int> reversedlist=new List<int>();
+   for (int i = eList.Count - 1; i >= 0; i--) {
+      reversedlist.Add(eList[i]);
+       
+   } 
+   return reversedlist;
+}
+
+List<int> reversednums =reverse(list);
+foreach(int item5 in reversednums){
+   Console.WriteLine(item5);
+}
